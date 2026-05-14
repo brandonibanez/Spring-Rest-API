7bf87f58843475e367968e18f201c6855529fce9
Brandon Ibañez <ibanezbrandonn@gmail.com>
2026-05-14 17:25:01 +0800

    Use record for error response DTO

--- src/main/java/com/example/demo/dto/ErrorResponseDTO.java
+++ src/main/java/com/example/demo/dto/ErrorResponseDTO.java
@@ -2,37 +2,7 @@ package com.example.demo.dto;
 
 import org.springframework.http.HttpStatus;
 
-public class ErrorResponseDTO {
+import java.time.LocalDateTime;
 
-    private HttpStatus statusCode;
-    private String message;
-
-    public ErrorResponseDTO(HttpStatus statusCode, String message) {
-        this.statusCode = statusCode;
-        this.message = message;
-    }
-
-    public HttpStatus getStatusCode() {
-        return statusCode;
-    }
-
-    public String getMessage() {
-        return message;
-    }
-
-    public void setStatusCode(HttpStatus statusCode) {
-        this.statusCode = statusCode;
-    }
-
-    public void setMessage(String message) {
-        this.message = message;
-    }
-
-    @Override
-    public String toString() {
-        return "ErrorResponseDTO{" +
-                "statusCode='" + statusCode + '\'' +
-                ", message='" + message + '\'' +
-                '}';
-    }
+public record ErrorResponseDTO(LocalDateTime timestamp, HttpStatus status , String message, String details) {
 }
